b06a512424c1f26cbc525c8a2b4205fa5455abb2
Gabriel Carvalho <gribeirocarvalho@hotmail.com>
2023-08-24 23:05:11 -0300

    Finalização do Projeto

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class CalculadoraDeIMC
+{
+    public static double CalcularIMC(double peso, double altura)
+    {
+        double imc = peso / (altura * altura);
+        return imc;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o peso (em kg): ");
+        double peso = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Informe a altura (em metros): ");
+        double altura = Convert.ToDouble(Console.ReadLine());
+
+        double imc  = CalculadoraDeIMC.CalcularIMC(peso, altura);
+
+        Console.WriteLine($"O seu IMC é: {imc:F2}");
+    }
+}
---
+++ instrucoes.md
@@ -0,0 +1,15 @@
+# Teste de Performance 2
+## Exercício #4
+
+Implemente um programa no qual o usuário informe o peso e a altura. Ao fim, o programa deve exibir o IMC.
+
+Requisitos:
+
+1. Regra para calcular IMC: peso / (altura * altura);
+
+Requisitos técnicos:
+
+2. Crie uma classe para representar o cálculo do IMC;
+3. Crie um método na classe (item 1) para calcular o IMC;
+4. Defina o método (item 2) como estático;
+5. Utilize a classe Console apenas no programa principal;
